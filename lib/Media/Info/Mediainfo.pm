d1aeaaaafce8b6533b16825535acea42176131b7
perlancar (on pc-office) <perlancar@gmail.com>
2017-07-10 20:50:25 +0700

    Replace Log::Any with Log::ger

--- lib/Media/Info/Mediainfo.pm
+++ lib/Media/Info/Mediainfo.pm
@@ -6,7 +6,7 @@ package Media::Info::Mediainfo;
 use 5.010001;
 use strict;
 use warnings;
-use Log::Any::IfLOG '$log';
+use Log::ger;
 
 use Capture::Tiny qw(capture);
 use IPC::System::Options 'system', -log=>1;
